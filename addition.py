544491e5a543645620024ba81f9425866a3f9576
yettyadeo <yetundeadeoye1@gmail.com>
2022-12-28 19:56:51 +0000

    1.txt file updated

---
+++ addition.py
@@ -0,0 +1,12 @@
+x = input("Type a number: ")
+y = input("Type another number: ")
+
+sum = int(x) + int(y)
+
+print("The sum is: ", sum)x = input("Type a number: ")
+y = input("Type another number: ")
+
+sum = int(x) + int(y)
+
+print("The sum is: ", sum)
+
